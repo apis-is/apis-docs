31448ecd68bd08a9d1a3f36b2e0eb777d721b17b
Benedikt D. Valdez Stefánsson <benediktvaldez@gmail.com>
2013-09-25 02:08:07 +0000

    A little cleanup

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 var express     = require('express'),
     http        = require('http'),
     less        = require('less'),
-    // mongoose    = require('mongoose'),
     colors      = require('colors'),
     logo        = require('./config/logo.js');
 
@@ -31,13 +30,6 @@ require('./bootstrap.js').boot(app);
 
 // -- Routes
 require('./config/routes.js')(app);
-
-// -- Models
-// require('./app/models/index.js')(app);
-
-// -- Mongo DB connection
-// mongoose.connect(process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/funso');
-
 
 var server = http.createServer(app);
 
